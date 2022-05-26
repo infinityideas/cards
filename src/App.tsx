b4b1331f9334053ded8b42e0d3b46ff9afb3da9f
Darius Sinha <dsinha@bbns.org>
2022-05-26 13:09:12 -0400

    Set rules and install pusher npm

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import HeaderText from './components/HeaderText';
 import NavButton from './components/NavButton';
 import Footer from './components/Footer';
 
+import logo from './assets/images/cards_logo.png';
+
 import './styles/App.css';
 
 class App extends React.Component<{}, {}> {
@@ -15,12 +17,17 @@ class App extends React.Component<{}, {}> {
 
         return (
             <div>
-                <HeaderText innerText="Cards" subText={subText}/>
-                <div id="ButtonHolder" style={{paddingBottom: "15px"}}>
-                   <NavButton innerText="Play" imageSRC="play" backgroundColor="#ed5645" toLink="/play"/>
-                   <NavButton innerText="About" imageSRC="about" backgroundColor="#0284e0" toLink="/about"/>
+                <div style={{textAlign: "center"}}>
+                    <img src={logo} style={{width: "30vw", marginTop: "20vh"}}/>
+                    <p style={{marginBottom: 50}}>Online multiplayer card games.</p>
+                </div>
+                <div id="ButtonHolder" style={{paddingBottom: "60px"}}>
+                   <NavButton innerText="Play" imageSRC="play" backgroundColor="#1fcc4d" toLink="/play"/>
                 </div>
                 <Footer/>
+                <div style={{width: "50%", margin: "auto", textAlign: "center"}}>
+                    <NavButton innerText="About" imageSRC="about" backgroundColor="#666666" toLink="/about"/>
+                </div>
             </div>
         );
     }
--- src/games/crazyeights.tsx
+++ src/games/crazyeights.tsx
@@ -2,27 +2,41 @@ import { Component } from 'react';
 import { Stage, Layer, Image } from 'react-konva';
 import RegDeck from '../scripts/CardDict';
 import useImage from 'use-image';
+import HeaderText from '../components/HeaderText';
+import React from 'react';
 
 const axios = require('axios');
 
 interface CrazyEightsState {
-  loaded: boolean
+  loaded: boolean,
+  rulesset: boolean,
 }
 
 const UrlImage = (props: any) => {
   const [image] = useImage(props.src);
-  return <Image image={image} x={props.x} y={props.y} width={props.width} height={props.height} draggable/>
+  return <Image image={image} x={props.x} y={props.y} width={props.width} height={props.height} draggable={props.draggable}/>
 }
 
 class CrazyEights extends Component<{}, CrazyEightsState> {
   private cardDeck: Array<any>;
+  private discardPile: Array<any>;
+  private hands: Array<any>;
+  private NUMPLAYERS: number;
+  private numPlayersRef: any;
 
   constructor(props: any) {
     super(props);
     this.state = {
-      loaded: false
+      loaded: false,
+      rulesset: false
     }
     this.cardDeck = [];
+    this.discardPile = [];
+    this.hands = [];
+    this.NUMPLAYERS = 3;
+    this.numPlayersRef = React.createRef();
+
+    this.setRules = this.setRules.bind(this);
   }
 
   componentDidMount() {
@@ -32,11 +46,26 @@ class CrazyEights extends Component<{}, CrazyEightsState> {
         for (var i=0; i<response['data']['data'].length; i++) {
           this.cardDeck.push(RegDeck[response['data']['data'][i]]);
         }
-        console.log(this.cardDeck);
+        for (var player = 0; player<this.NUMPLAYERS; player++) {
+          this.hands.push([]);
+
+        }
         this.setState({
           loaded: true
         })
       });
+    }
+  }
+
+  setRules() {
+    if (this.numPlayersRef.current.value < 2 || this.numPlayersRef.current.value > 4 || !this.numPlayersRef.current.value) {
+      alert("You can only have between 2 and 4 players, inclusive!");
+    } else {
+      this.NUMPLAYERS = this.numPlayersRef.current.value;
+      this.setState({
+        loaded: true,
+        rulesset: true
+      })
     }
   }
 
@@ -46,14 +75,33 @@ class CrazyEights extends Component<{}, CrazyEightsState> {
         <div>Loading</div>
       )
     }
+    if (!this.state.rulesset) {
+      return (
+        <div>
+          <HeaderText innerText="Crazy Eights" subText={<p>Set the rules for the game here.</p>}/>
+          <div style={{textAlign: "center"}}>
+            <label htmlFor='numplayers' style={{paddingRight: 10}}>How many Players?</label>
+            <input type="number" name="numplayers" ref={this.numPlayersRef} /><br/><br/>
+            <button onClick={this.setRules}>Set Rules</button>
+          </div>
+        </div>
+      )
+    }
     return (
       <Stage width={window.innerHeight*1.5} height={window.innerHeight}>
         <Layer>
-          <UrlImage src={this.cardDeck[0].image} x={(window.innerHeight*1.5)/2-40} y={window.innerHeight-232} width={160} height={232}/>
-          <UrlImage src={this.cardDeck[1].image} x={(window.innerHeight*1.5)/2-20} y={window.innerHeight-232} width={160} height={232}/>
-          <UrlImage src={this.cardDeck[2].image} x={(window.innerHeight*1.5)/2-0} y={window.innerHeight-232} width={160} height={232}/>
-          <UrlImage src={this.cardDeck[3].image} x={(window.innerHeight*1.5)/2+20} y={window.innerHeight-232} width={160} height={232}/>
-          <UrlImage src={this.cardDeck[4].image} x={(window.innerHeight*1.5)/2+40} y={window.innerHeight-232} width={160} height={232}/>
+          <UrlImage src="https://infinitecards.s3.us-east-1.amazonaws.com/Card_back_01.svg.png" x={(window.innerHeight*1.5)/2-60} y={(window.innerHeight/2)-87} width={120} height={174} />
+          <UrlImage src={this.cardDeck[0].image} x={(window.innerHeight*1.5)/2-40} y={window.innerHeight-174} width={120} height={174} draggable/>
+          <UrlImage src={this.cardDeck[1].image} x={(window.innerHeight*1.5)/2-20} y={window.innerHeight-174} width={120} height={174}/>
+          <UrlImage src={this.cardDeck[2].image} x={(window.innerHeight*1.5)/2-0} y={window.innerHeight-174} width={120} height={174}/>
+          <UrlImage src={this.cardDeck[3].image} x={(window.innerHeight*1.5)/2+20} y={window.innerHeight-174} width={120} height={174}/>
+          <UrlImage src={this.cardDeck[4].image} x={(window.innerHeight*1.5)/2+40} y={window.innerHeight-174} width={120} height={174}/>
+
+          <UrlImage src={this.cardDeck[5].image} x={(window.innerHeight*1.5)/2-40} y={0} width={120} height={174}/>
+          <UrlImage src={this.cardDeck[6].image} x={(window.innerHeight*1.5)/2-20} y={0} width={120} height={174}/>
+          <UrlImage src={this.cardDeck[7].image} x={(window.innerHeight*1.5)/2-0} y={0} width={120} height={174}/>
+          <UrlImage src={this.cardDeck[8].image} x={(window.innerHeight*1.5)/2+20} y={0} width={120} height={174}/>
+          <UrlImage src={this.cardDeck[9].image} x={(window.innerHeight*1.5)/2+40} y={0} width={120} height={174}/>
         </Layer>
       </Stage>
     );
